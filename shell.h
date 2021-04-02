364cb71a7a2e5a5c2fed4280b462744a8e2b201b
SimonBr017 <simonbrard017@gmail.com>
2021-04-02 13:35:13 +0000

    add shell.h fonction and modif main

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
