2b1af29e1afc83eb90e84ccbb68c3948c1b134f9
Etienne Brun <etienne.brunj@gmail.com>
2021-04-02 11:08:14 +0000

    working with ls

--- strtok.c
+++ strtok.c
@@ -12,44 +12,57 @@ void ctrlc(int i)
 
 int main()
 {
-	char *argv[5];
+	char *argv[100];
+	char *token;
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	int characters;
-	pid_t my_pid;
-	char *token;
+	pid_t pid;
 	int i = 0;
-	
+	char* path= "/bin/";
+	char progpath[20];
+
 	do
 	{
 		signal(SIGINT, ctrlc);
 
-		printf("----------------------------------------\n");
-		my_pid = getpid();
-		printf("pid: %u\n", my_pid);
-		my_pid = getppid();
-                printf("ppid: %u\n", my_pid);
-
+//		printf("----------------------------------------\n");
+/*		my_pid = getpid();
+ *		printf("pid: %u\n", my_pid);
+ *		my_pid = getppid();
+ *              printf("ppid: %u\n", my_pid);
+ */
 		write(1, "$ ",2);
 
 		characters = getline(&buffer, &bufsize, stdin);
-	
-		printf("%s\n", buffer);
-		
+
+//		printf("%s\n", buffer);
+
 		token = strtok(buffer, " ");
 		while(token != NULL)
 		{
-			printf("arg: %s\n",token);
+//			printf("arg: %s\n",token);
 			argv[i] = token;
 			token = strtok(NULL," ");
-			i++;			
+			i++;
 		}
-				       		
-		printf("%i characters\n", characters);
-		
-	} while(buffer != NULL && characters != -1);
+//		printf("%i characters\n", characters);
+		argv[i] = NULL;
 
-	free(buffer);	
-	printf("exit sucsess !!\n");  
+		strcpy(progpath, path);
+		strcat(progpath, argv[0]);
+
+		if (pid == fork() == 0)
+			if ((execvp(progpath, argv) == -1))
+				perror("Execvp == -1: Error");
+			else
+			{
+				wait(NULL);
+				printf("Child a finit");
+			}
+	} while (buffer != NULL && characters != -1);
+
+	free(buffer);
+	printf("exit sucsess !!\n");
 	return(0);
 }
