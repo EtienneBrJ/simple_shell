0e1f752c4d9a8c1d201b736c956b558c247b4d3f
SimonBr017 <simonbrard017@gmail.com>
2021-04-06 10:49:49 +0000

    fixed dos end of line

--- action.c
+++ action.c
@@ -3,7 +3,7 @@
 char *_getenv(char *var_env_name)
 {
 	int i = 0;
-	
+
 	while (*environ[i])
 	{
 		if (_strstr(environ[i], var_env_name) != NULL)
@@ -18,32 +18,28 @@ void _execute(char **argv)
 	int status;
 	pid_t pid;
 	char *command_to_execute;
-	
-	
+
 	command_to_execute = _which(argv[0]);
 	if (command_to_execute != NULL)
 		argv[0] = command_to_execute;
 
-
 	pid = fork();
-	
+
 	if (pid == 0)
 	{
-		
+
 		if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror(argv[0]);
-				free(command_to_execute);
-				free_double_ptr(argv);
-				exit(0);
-			}
-	}	
+		{
+			perror(argv[0]);
+			free(command_to_execute);
+			/*free_double_ptr(argv);*/
+			exit(0);
+		}
+	}
 
 	else
 		wait(&status);
-	free(command_to_execute);
-	
-		
+	/*free(command_to_execute);*/
 }
 
 char *_which(char *command_name)
@@ -58,8 +54,6 @@ char *_which(char *command_name)
 	char *delimiter = "=:";
 	int length = 0;
 
-
-
 	if (command_name == NULL)
 	{
 		return (NULL);
@@ -67,7 +61,7 @@ char *_which(char *command_name)
 	if (stat(command_name, &st) != 0)
 	{
 		path = _getenv("PATH");
-		
+
 		if (path == NULL)
 		{
 			perror("Invalid path");
@@ -76,14 +70,14 @@ char *_which(char *command_name)
 		length = _strlen(path);
 
 		pathcp = malloc((sizeof(char) * length) + 1);
-		if(pathcp == NULL)
-			return(NULL);
+		if (pathcp == NULL)
+			return (NULL);
 
 		_strncpy(pathcp, path, length);
 
 		list_path = malloc(sizeof(char) * length);
 		if (list_path == NULL)
-			return(NULL);
+			return (NULL);
 		parseString(pathcp, list_path, delimiter);
 
 		while (list_path[i])
@@ -96,23 +90,20 @@ char *_which(char *command_name)
 			absolute_path = "";
 			absolute_path = str_concat(absolute_path, list_path[i]);
 			absolute_path = str_concat(absolute_path, command_name);
-			
-					
+
 			if (stat(absolute_path, &st) == 0)
 			{
-				
+
 				return (absolute_path);
 			}
 			i++;
-
-		
 		}
 		free_double_ptr(list_path);
 		free(pathcp);
-	}	
+	}
 	else
 	{
-		 return (command_name);
+		return (command_name);
 	}
 	return (NULL);
 }
--- main.c
+++ main.c
@@ -5,50 +5,51 @@ int main(void)
 	char *buffer;
 	size_t bufsize;
 	char *delim = " \n\t\r\f\v";
-	char **argv; 
-	
+	char **argv;
+	int prompt = 1;
+
 	buffer = NULL;
 	bufsize = 0;
-	
-	while (1)
+
+	while (prompt)
 	{
 		if (isatty(STDIN_FILENO))
 		{
 			write(STDOUT_FILENO, "$ ", 2);
 		}
-		
+
 		signal(SIGINT, ctrlc);
-		
+
 		if (getline(&buffer, &bufsize, stdin) == EOF)
 		{
 			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO,"\n",1);
+				write(STDOUT_FILENO, "\n", 1);
 			free(buffer);
 			exit(EXIT_SUCCESS);
 		}
 		/*rm_last_char_if(buffer);*/
-		
- 		if(_strcmp(buffer, "exit") == 0)
- 		{
+
+		if (_strcmp(buffer, "exit") == 0)
+		{
 			free(buffer);
 			exit(EXIT_SUCCESS);
- 		}
-		if(_strcmp(buffer, "env") == 0)
+		}
+		if (_strcmp(buffer, "env") == 0)
 		{
 			free(buffer);
 			/*free_double_ptr(argv);*/
 			print_environment(environ);
-		}	
-		
-	 	argv = malloc(sizeof(_strlen(buffer)));
+		}
+
+		argv = malloc(sizeof(_strlen(buffer)));
 		if (argv == NULL)
 			exit(EXIT_FAILURE);
-		
+
 		parseString(buffer, argv, delim);
 		if (argv[0] == NULL)
 			continue;
-		else	
+		else
 			_execute(argv);
-		}
-	return(0);
-}		
+	}
+	return (0);
+}
